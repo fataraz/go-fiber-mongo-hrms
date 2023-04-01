580c5051440fff89952057042b2f34ef6dd9f1e8
Razif <fatahillah.razif@gmail.com>
2023-04-01 17:05:49 +0700

    create delete

--- main.go
+++ main.go
@@ -119,7 +119,22 @@ func main() {
 		return c.Status(200).JSON(employee)
 	})
 
-	app.Delete("/employee/:id")
+	app.Delete("/employee/:id", func(c *fiber.Ctx) error {
+		employeeID, err := primitive.ObjectIDFromHex(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+		result, err := mg.Db.Collection("employees").DeleteOne(c.Context(), query)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+		if result.DeletedCount < 1 {
+			return c.SendStatus(404)
+		}
+		return c.Status(200).JSON("record deleted")
+	})
 
 	log.Fatal(app.Listen(":3000"))
 }
